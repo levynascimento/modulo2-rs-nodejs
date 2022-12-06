270e10ea74406d1a4348211f9203020c484f2015
Gustavo Henrique Ribeiro Dias <gugahribeiro@hotmail.com>
2022-12-06 15:48:25 -0300

    lint: all files inside src

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,16 @@
-import { Router } from "express";
-import { CategoryRepositories } from "../repositories/CategoriesRepository";
+import { Router } from 'express';
+
+import { CategoryRepositories } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoryRepositories
+const categoriesRepository = new CategoryRepositories();
 
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
 
-categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body
-
-  categoriesRepository.create({ name, description })
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
 });
 
 export { categoriesRoutes };
-
