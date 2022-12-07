728dd667543345ae844f81949c90c82c5044d137
Levy Martins Nascimento <levymnascimento@hotmail.com>
2022-12-07 09:10:32 -0300

    Corrigido código

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -6,24 +6,33 @@ interface ICreateCategoryDTO {
 }
 
 class CategoryRepositories {
-
-  private categories: Category[]
+  private categories: Category[];
 
   constructor() {
     this.categories = [];
   }
 
   create({ name, description }: ICreateCategoryDTO): void {
-    const category = new Category()
+    const category = new Category();
 
     Object.assign(category, {
       name,
       description,
-      created_at: new Date()
-    })
+      created_at: new Date(),
+    });
 
     this.categories.push(category);
   }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    const category = this.categories.find(category => category.name === name);
+
+    return category;
+  }
 }
 
-export { CategoryRepositories }
+export { CategoryRepositories };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,28 @@
-import { Router } from "express";
-import { CategoryRepositories } from "../repositories/CategoriesRepository";
+import { Router } from 'express';
+
+import { CategoryRepositories } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoryRepositories
+const categoriesRepository = new CategoryRepositories();
 
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
 
-categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-  categoriesRepository.create({ name, description })
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: 'Category already exists!' });
+  }
+
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
 });
 
-export { categoriesRoutes };
+categoriesRoutes.get('/', (request, response) => {
+  const allCategories = categoriesRepository.list();
 
+  return response.json(allCategories);
+});
+
+export { categoriesRoutes };
