39238d6de67eab13608e7d91622e1b131b59ec77
Levy Martins Nascimento <97737623+levynascimento@users.noreply.github.com>
2022-12-07 09:02:35 -0300

    Revert "Setup eslint"

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,17 @@
-import { Router } from 'express';
-
-import { CategoryRepositories } from '../repositories/CategoriesRepository';
+import { Router } from "express";
+import { CategoryRepositories } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoryRepositories();
+const categoriesRepository = new CategoryRepositories
 
-categoriesRoutes.post('/', (request, response) => {
-  const { name, description } = request.body;
 
-  categoriesRepository.create({ name, description });
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body
+
+  categoriesRepository.create({ name, description })
 
   return response.status(201).send();
 });
 
 export { categoriesRoutes };
+
