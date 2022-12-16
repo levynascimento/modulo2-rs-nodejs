a504297c51a2611c0868d86f6b41e9bd0ea95a15
Levy Martins Nascimento <levymnascimento@hotmail.com>
2022-12-16 13:36:16 -0300

    Tentando fazer conexão com o banco

---
+++ src/database/index.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'docker',
+  password: 'ignite',
+  database: 'database_ignite',
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch(err => {
+    console.error('Error during Data Source initialization', err);
+  });
