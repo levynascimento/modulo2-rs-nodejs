305d7e2d9dae258b16102e1fe3f5a89138455426
Levy Martins Nascimento <levymnascimento@hotmail.com>
2023-03-06 10:39:44 -0300

    Adicionado CORS

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,12 +1,12 @@
 import 'dotenv/config';
 import 'reflect-metadata';
+import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-import upload from '@config/upload';
-
 import '@shared/container';
+import upload from '@config/upload';
 import { AppError } from '@shared/errors/AppError';
 
 import swaggerFile from '../../../swagger.json';
@@ -21,6 +21,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
